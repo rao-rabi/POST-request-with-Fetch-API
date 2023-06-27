f61f5eb71bd7e90c02c6514dcdd86dc1d2006b0f
rao rabi <rabirao1039@gmail.com>
2023-06-27 10:42:11 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let createTodo = async (todo) => {
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(todo)
+    }
+    let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+    let response = await p.json()
+    return response
+}
+
+let getTodo = async (id) => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+    let todo = await response.json();
+    return todo;
+}
+
+let mainFunction = async () => {
+    let todo = {
+        title: 'Rabi',
+        body: 'Roll NO.18',
+        userId: '2021-GCUF-05050',
+    };
+    let todoResponse = await createTodo(todo)
+    console.log(todoResponse);
+    console.log(await getTodo(21));
+}
+
+mainFunction()
+
